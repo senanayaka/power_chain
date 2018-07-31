88822b84768b6c25006eeb8c3bcae5a3cd1dd8ee
Parakrama <senanayakaparakrama@gmail.com>
2018-07-31 12:27:19 +0800

    add faker and powe route to test the app

---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+var faker = require("faker");
+
+var appRouter = function (app) {
+    app.get("/", function(req, res) {
+        res.status(200).send("Block chain for power data");
+    });
+
+
+    app.get("/power/:num", function (req, res) {
+        var users = [];
+        var num = req.params.num;
+
+        if (isFinite(num) && num  > 0 ) {
+            for (i = 0; i <= num-1; i++) {
+                users.push({
+                    product: faker.company.companyName(),
+                    place: faker.address.zipCode(),
+                    power_outlet: faker.random.number()
+                });
+            }
+
+            res.status(200).send(users);
+
+        } else {
+            res.status(400).send({ message: 'invalid number supplied' });
+        }
+
+    });
+
+}
+
+
+
+module.exports = appRouter;
